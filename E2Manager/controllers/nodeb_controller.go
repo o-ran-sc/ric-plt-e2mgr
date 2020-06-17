d6d623cc2eda63be912b4e322c25e492abe86a99
Irina <ib565x@intl.att.com>
2020-06-17 15:01:21 +0300

    [RIC-395] - E2M new REST API - E2M Set Parameters - Automation + Decoder
    Change-Id: I4cefd6da49c6e67103ad07d2612b41aa6f955fbf
    Signed-off-by: Irina <ib565x@intl.att.com>

--- E2Manager/controllers/nodeb_controller.go
+++ E2Manager/controllers/nodeb_controller.go
@@ -103,7 +103,7 @@ func (c *NodebController) SetGeneralConfiguration(writer http.ResponseWriter, r 
 
 	request := models.GeneralConfigurationRequest{}
 
-	if !c.extractJsonBody(r, &request, writer){
+	if !c.extractJsonBodyDisallowUnknownFields(r, &request, writer){
 		return
 	}
 	c.handleRequest(writer, &r.Header, httpmsghandlerprovider.SetGeneralConfigurationRequest, request, false)
@@ -157,6 +157,21 @@ func (c *NodebController) extractRequestBodyToProto(r *http.Request, pb proto.Me
 	err := jsonpb.Unmarshal(r.Body, pb)
 
 	if err != nil {
+		c.logger.Errorf("[Client -> E2 Manager] #NodebController.extractJsonBody - unable to extract json body - error: %s", err)
+		c.handleErrorResponse(e2managererrors.NewInvalidJsonError(), writer)
+		return false
+	}
+
+	return true
+}
+
+func (c *NodebController) extractJsonBodyDisallowUnknownFields(r *http.Request, request models.Request, writer http.ResponseWriter) bool {
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&request); err != nil {
 		c.logger.Errorf("[Client -> E2 Manager] #NodebController.extractJsonBody - unable to extract json body - error: %s", err)
 		c.handleErrorResponse(e2managererrors.NewInvalidJsonError(), writer)
 		return false
