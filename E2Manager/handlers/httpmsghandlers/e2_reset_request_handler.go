c69188285e89b5cb4839d313a13c23c295f3c4ba
naman.gupta <naman.gupta@samsung.com>
2023-05-30 17:22:02 +0530

    Changing status to connected state after timeout.
    Changing status to connected state after timeout.
    
    Signed-off-by: naman.gupta <naman.gupta@samsung.com>
    Change-Id: Ie9e09b4576c537e560a897286b5fb0c1491366ac

--- E2Manager/handlers/httpmsghandlers/e2_reset_request_handler.go
+++ E2Manager/handlers/httpmsghandlers/e2_reset_request_handler.go
@@ -25,24 +25,27 @@ import (
 	"e2mgr/models"
 	"e2mgr/services"
 	"e2mgr/services/rmrsender"
+	"time"
 
 	"gerrit.o-ran-sc.org/r/ric-plt/nodeb-rnib.git/common"
 	"gerrit.o-ran-sc.org/r/ric-plt/nodeb-rnib.git/entities"
 )
 
 type E2ResetRequestHandler struct {
-	rNibDataService services.RNibDataService
-	rmrSender       *rmrsender.RmrSender
-	logger          *logger.Logger
-	ranResetManager *managers.RanResetManager
+	rNibDataService                   services.RNibDataService
+	rmrSender                         *rmrsender.RmrSender
+	logger                            *logger.Logger
+	ranResetManager                   *managers.RanResetManager
+	changeStatusToConnectedRanManager *managers.ChangeStatusToConnectedRanManager
 }
 
-func NewE2ResetRequestHandler(logger *logger.Logger, rmrSender *rmrsender.RmrSender, rNibDataService services.RNibDataService, ranResetManager *managers.RanResetManager) *E2ResetRequestHandler {
+func NewE2ResetRequestHandler(logger *logger.Logger, rmrSender *rmrsender.RmrSender, rNibDataService services.RNibDataService, ranResetManager *managers.RanResetManager, changeStatusToConnectedRanManager *managers.ChangeStatusToConnectedRanManager) *E2ResetRequestHandler {
 	return &E2ResetRequestHandler{
-		rNibDataService: rNibDataService,
-		rmrSender:       rmrSender,
-		logger:          logger,
-		ranResetManager: ranResetManager,
+		rNibDataService:                   rNibDataService,
+		rmrSender:                         rmrSender,
+		logger:                            logger,
+		ranResetManager:                   ranResetManager,
+		changeStatusToConnectedRanManager: changeStatusToConnectedRanManager,
 	}
 }
 
@@ -51,7 +54,7 @@ func (e *E2ResetRequestHandler) Handle(request models.Request) error {
 	e.logger.Infof("#E2ResetRequestHandler.Handle - Ran name: %s", resetRequest.RanName)
 	nodebInfo, err := e.getNodebInfo(resetRequest.RanName)
 	if err != nil {
-		e.logger.Errorf("#E2ResetRequestNotificationHandler.Handle - failed to get status of RAN: %s from RNIB. Error: %s", resetRequest.RanName, err.Error())
+		e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to get status of RAN: %s from RNIB. Error: %s", resetRequest.RanName, err.Error())
 		_, ok := err.(*common.ResourceNotFoundError)
 		if ok {
 			return e2managererrors.NewResourceNotFoundError()
@@ -59,25 +62,47 @@ func (e *E2ResetRequestHandler) Handle(request models.Request) error {
 		return e2managererrors.NewRnibDbError()
 	}
 
-	e.logger.Debugf("#E2ResetRequestNotificationHandler.Handle - nodeB entity retrieved. RanName %s, ConnectionStatus %s", nodebInfo.RanName, nodebInfo.ConnectionStatus)
+	e.logger.Debugf("#E2ResetRequestHandler.Handle - nodeB entity retrieved. RanName %s, ConnectionStatus %s", nodebInfo.RanName, nodebInfo.ConnectionStatus)
 
 	ranName := resetRequest.RanName
 	isResetDone, err := e.ranResetManager.ResetRan(ranName)
 	if err != nil {
-		e.logger.Errorf("#E2ResetRequestNotificationHandler.Handle - failed to update and notify connection status of nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err.Error())
+		e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to update and notify connection status of nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err.Error())
 		return err
 	} else {
 		if isResetDone {
 			nodebInfoupdated, err1 := e.getNodebInfo(resetRequest.RanName)
 			if err1 != nil {
-				e.logger.Errorf("#E2ResetRequestNotificationHandler.Handle - failed to get updated nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err1.Error())
+				e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to get updated nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err1.Error())
 				return err1
 			}
-			e.logger.Debugf("#E2ResetRequestNotificationHandler.Handle - Reset Done Successfully ran: %s , Connection status updated : %s", ranName, nodebInfoupdated.ConnectionStatus)
+			e.logger.Debugf("#E2ResetRequestHandler.Handle - Reset Done Successfully ran: %s , Connection status updated : %s", ranName, nodebInfoupdated.ConnectionStatus)
 		} else {
-			e.logger.Debugf("#E2ResetRequestNotificationHandler.Handle - Reset Failed")
+			e.logger.Debugf("#E2ResetRequestHandler.Handle - Reset Failed")
 		}
 	}
+
+	//Todo: add timer
+	time.Sleep(5 * time.Second)
+
+	isConnectedStatus, err := e.changeStatusToConnectedRanManager.ChangeStatusToConnectedRan(ranName)
+	if err != nil {
+		e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to update and notify connection status of nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err.Error())
+		return err
+	} else {
+		if isConnectedStatus {
+			nodebInfoupdated, err1 := e.getNodebInfo(resetRequest.RanName)
+			if err1 != nil {
+				e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to get updated nodeB entity. RanName: %s. Error: %s", resetRequest.RanName, err1.Error())
+				return err1
+			}
+			e.logger.Debugf("#E2ResetRequestHandler.Handle - Connection status Set Successfully ran: %s , Connection status updated : %s", ranName, nodebInfoupdated.ConnectionStatus)
+		} else {
+			e.logger.Debugf("#E2ResetRequestHandler.Handle - Connection status Setting Failed")
+		}
+	}
+
+	e.logger.Debugf("#E2ResetRequestHandler.Handle - nodeB entity connected state. RanName %s, ConnectionStatus %s", nodebInfo.RanName, nodebInfo.ConnectionStatus)
 
 	return nil
 }
@@ -86,7 +111,7 @@ func (e *E2ResetRequestHandler) getNodebInfo(ranName string) (*entities.NodebInf
 
 	nodebInfo, err := e.rNibDataService.GetNodeb(ranName)
 	if err != nil {
-		e.logger.Errorf("#E2ResetRequestNotificationHandler.Handle - failed to retrieve nodeB entity. RanName: %s. Error: %s", ranName, err.Error())
+		e.logger.Errorf("#E2ResetRequestHandler.Handle - failed to retrieve nodeB entity. RanName: %s. Error: %s", ranName, err.Error())
 		return nil, err
 	}
 	return nodebInfo, nil
